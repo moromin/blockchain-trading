1eba9b6db1ce1b892ed2ec0f715e52c8ea93d490
moromin <34294957+moromin@users.noreply.github.com>
2021-10-01 19:47:52 +0900

    Update: realTimeTicker service, jsonRPC entity

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"blockchain-trading/config"
+	"blockchain-trading/model/entity"
 	"blockchain-trading/model/repository"
 	"blockchain-trading/service"
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -15,6 +17,15 @@ func main() {
 	balance := service.NewBalanceService(apiClient)
 	fmt.Println(balance.GetBalance())
 
-	ticker := service.NewTickerService(apiClient)
-	fmt.Println(ticker.GetTicker(config.Env.ProductCode))
+	// ticker := service.NewTickerService(apiClient)
+	// fmt.Println(ticker.GetTicker(config.Env.ProductCode))
+
+	tickerChannel := make(chan entity.Ticker)
+	realTimeTicker := service.NewRealTimeTickerService(apiClient)
+	go realTimeTicker.GetRealTimeTicker(config.Env.ProductCode, tickerChannel)
+	for ticker := range tickerChannel {
+		fmt.Println(ticker.GetMidPrice())
+		fmt.Println(ticker.DateTime())
+		fmt.Println(ticker.TruncateDateTime(time.Hour))
+	}
 }
