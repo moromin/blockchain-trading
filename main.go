36cddf6a1ef80f537028f0968c18a1dca10c45c3
moromin <34294957+moromin@users.noreply.github.com>
2021-11-12 00:33:19 +0900

    Update: call injector in main.go

--- main.go
+++ main.go
@@ -1,63 +1,19 @@
 package main
 
 import (
-	"blockchain-trading/config"
-	"fmt"
-	"strconv"
-	"time"
+	"blockchain-trading/injector"
 
-	"code.cryptowat.ch/cw-sdk-go/client/rest"
-	"code.cryptowat.ch/cw-sdk-go/common"
+	"github.com/davecgh/go-spew/spew"
 )
 
 const dateFormat = "2006-01-02 15:04:05"
 
 func main() {
-	// fmt.Println(config.Env)
+	exchangeUsecase := injector.InjectExchangeUsecase()
 
-	// bfTarget := api.Target{
-	// 	BaseURL: api.BitFlyerURL,
-	// 	Header: map[string]string{
-	// 		"ACCESS-KEY":   config.Env.BfKey,
-	// 		"Content-Type": "application/json",
-	// 	},
-	// }
-	// bfClient := api.NewAPIClient(bfTarget)
+	balance, _ := exchangeUsecase.ConfirmBalace()
+	spew.Dump(balance)
 
-	// balance := repository.NewBalanceRepository(bfClient)
-	// fmt.Println(balance.GetBalance())
-
-	// ticker := repository.NewTickerRepository(bfClient)
-	// fmt.Println(ticker.GetTicker(config.Env.ProductCode))
-
-	// tickerChannel := make(chan entity.Ticker)
-	// realTimeTicker := repository.NewRealTimeTickerRepository(bfClient)
-	// go realTimeTicker.GetRealTimeTicker(config.Env.ProductCode, tickerChannel)
-	// for ticker := range tickerChannel {
-	// 	fmt.Println(ticker.GetMidPrice())
-	// 	fmt.Println(ticker.DateTime())
-	// 	fmt.Println(ticker.TruncateDateTime(time.Hour))
-	// }
-
-	cwParams := rest.RESTClientParams{
-		URL:    "", // If URL is empty, the default RESTURL will be specified.
-		APIKey: config.Env.CwKey,
-	}
-	cwClient := rest.NewRESTClient(&cwParams)
-	after, err := time.Parse(dateFormat, "2021-11-05 00:00:00")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	cwQuery := map[string]string{
-		"after":   strconv.Itoa(int(after.Unix())),
-		"periods": string(common.Period1H),
-	}
-	// fmt.Println(cwQuery)
-	ohlc, err := cwClient.GetOHLC("bitflyer", "btcjpy", cwQuery)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("%+v\n", ohlc)
+	ticker, _ := exchangeUsecase.ViewTicker("BTC_JPY")
+	spew.Dump(ticker)
 }
