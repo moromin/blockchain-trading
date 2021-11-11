be36cdd931ebd1d5eab161a3347aba91801e6444
moromin <34294957+moromin@users.noreply.github.com>
2021-11-12 00:30:53 +0900

    Update: order struct name

--- domain/model/order.go
+++ domain/model/order.go
@@ -1,6 +1,6 @@
 package model
 
-type Order struct {
+type OrderData struct {
 	ProductCode    string  `json:"product_code"`
 	ChildOrderType string  `json:"child_order_type"`
 	Side           string  `json:"side"`
@@ -10,6 +10,6 @@ type Order struct {
 	TimeInForce    string  `json:"time_in_force"`
 }
 
-type ResponseChildOrder struct {
+type Order struct {
 	ChildOrderAcceptanceID string `json:"child_order_acceptance_id"`
 }
