84c2f74deb9b1c56ca30c53b3763bf9329b8a05b
moromin <34294957+moromin@users.noreply.github.com>
2021-11-21 11:09:57 +0900

    Update: call function from di container.Invoke

--- main.go
+++ main.go
@@ -2,28 +2,40 @@ package main
 
 import (
 	"blockchain-trading/config"
-	"blockchain-trading/infra"
-	"blockchain-trading/usecase"
-
-	"github.com/davecgh/go-spew/spew"
+	"blockchain-trading/di"
+	"blockchain-trading/infrastructure"
+	"blockchain-trading/interfaces/exchange"
+	"blockchain-trading/interfaces/presenter"
+	"fmt"
+	"net/url"
 )
 
 // Use this when call Cryptowatch SDK methods to get past OHLC data.
 const dateFormat = "2006-01-02 15:04:05"
 
 func main() {
-	target := infra.Target{
-		BaseURL: infra.BitFlyerURL,
+	baseURL, err := url.Parse(exchange.BitFlyerURL)
+	if err != nil {
+		panic(err)
+	}
+
+	target := infrastructure.Target{
+		BaseURL: baseURL,
 		Header: map[string]string{
 			"ACCESS-KEY":   config.Env.BfKey,
 			"Content-Type": "application/json",
 		},
 	}
 
-	ac := infra.NewAPIClient(target)
-	er := infra.NewExchangeRepository(ac)
-	es := usecase.NewExchangeUsecase(er)
-
-	balance, _ := es.ConfirmBalace()
-	spew.Dump(balance)
+	container, err := di.New(target)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err := container.Invoke(func(e *presenter.ExchangePresenter) {
+		e.ShowBalance()
+	}); err != nil {
+		fmt.Println(err)
+		return
+	}
 }
