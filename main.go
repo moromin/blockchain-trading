528261088c8048b8f33838802d68cabfcb35cd2c
moromin <34294957+moromin@users.noreply.github.com>
2021-11-28 14:27:53 +0900

    Add: confirm OHLC data

--- main.go
+++ main.go
@@ -8,13 +8,39 @@ import (
 	"blockchain-trading/interfaces/presenter"
 	"fmt"
 	"net/url"
+
+	_ "github.com/lib/pq"
 )
 
-// Use this when call Cryptowatch SDK methods to get past OHLC data.
-const dateFormat = "2006-01-02 15:04:05"
-
 func main() {
-	baseURL, err := url.Parse(exchange.BitFlyerURL)
+	// Confirm apiclient
+	// baseURL, err := url.Parse(exchange.BitFlyerURL)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// target := infrastructure.Target{
+	// 	BaseURL: baseURL,
+	// 	Header: map[string]string{
+	// 		"ACCESS-KEY":   config.Env.BfKey,
+	// 		"Content-Type": "application/json",
+	// 	},
+	// }
+
+	// container, err := di.New(target)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// if err := container.Invoke(func(e *presenter.ExchangePresenter) {
+	// 	e.ShowBalance()
+	// }); err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// Confirm ohlc data
+	baseURL, err := url.Parse(exchange.BinanceURL)
 	if err != nil {
 		panic(err)
 	}
@@ -22,7 +48,7 @@ func main() {
 	target := infrastructure.Target{
 		BaseURL: baseURL,
 		Header: map[string]string{
-			"ACCESS-KEY":   config.Env.BfKey,
+			"ACCESS-KEY":   config.Env.BinanceKey,
 			"Content-Type": "application/json",
 		},
 	}
@@ -32,10 +58,25 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	if err := container.Invoke(func(e *presenter.ExchangePresenter) {
-		e.ShowBalance()
+
+	query := map[string]string{
+		"symbol":   "BTCUSDT",
+		"interval": exchange.Interval1m,
+	}
+
+	if err := container.Invoke(func(d *presenter.ExchangePresenter) {
+		d.ShowOHLC(query)
 	}); err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	// Confirm DB
+	// conn, err := sql.Open("postgres", "user=root password=secret host=localhost dbname=test sslmode=disable")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// handler := infrastructure.SqlHandler{Conn: conn}
+	// _, err = di.NewDB(handler)
+
 }
