f06be1668a2aef875891b0d765e70479018eb7c4
moromin <34294957+moromin@users.noreply.github.com>
2021-12-01 00:42:29 +0900

    Fix: cnflict in di.go

--- di/di.go
+++ di/di.go
@@ -3,6 +3,7 @@ package di
 import (
 	"blockchain-trading/infrastructure"
 	"blockchain-trading/interfaces/api"
+	"blockchain-trading/interfaces/database"
 	"blockchain-trading/interfaces/exchange"
 	"blockchain-trading/interfaces/presenter"
 	"blockchain-trading/usecase"
@@ -53,15 +54,33 @@ func New(target infrastructure.Target) (*dig.Container, error) {
 	return c, nil
 }
 
-// func NewDB(handler infrastructure.SqlHandler) (*dig.Container, error) {
-// 	c := dig.New()
+func NewDB(handler infrastructure.SqlHandler) (*dig.Container, error) {
+	c := dig.New()
 
-// 	// sql handler
-// 	if err := c.Provide(func() infrastructure.SqlHandler {
-// 		return handler
-// 	}); err != nil {
-// 		return nil, errors.WithStack(err)
-// 	}
+	// sql handler
+	if err := c.Provide(func(dbi *usecase.DatabaseInteractor) *presenter.DatabasePresenter {
+		return &presenter.DatabasePresenter{Interactor: dbi}
+	}); err != nil {
+		return nil, errors.WithStack(err)
+	}
 
-// 	return c, nil
-// }
+	if err := c.Provide(func(dbRepo *database.DatabaseRepository) *usecase.DatabaseInteractor {
+		return &usecase.DatabaseInteractor{DatabaseRepository: dbRepo}
+	}); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	if err := c.Provide(func(sh database.SqlHandler) *database.DatabaseRepository {
+		return &database.DatabaseRepository{SqlHandler: sh}
+	}); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	if err := c.Provide(func() database.SqlHandler {
+		return &handler
+	}); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return c, nil
+}
