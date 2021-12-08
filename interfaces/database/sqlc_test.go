5d11576ad8511ec57fe4f23b86b22e298658cb52
moromin <34294957+moromin@users.noreply.github.com>
2021-12-08 18:41:35 +0900

    Update: test ListCurrencies() and GetCurrency()

--- interfaces/database/sqlc_test.go
+++ interfaces/database/sqlc_test.go
@@ -45,6 +45,34 @@ func TestResisterCurrency(t *testing.T) {
 	createRandomCurrency(t)
 }
 
+func TestGetCurrency(t *testing.T) {
+	currency1 := createRandomCurrency(t)
+	currency2, err := testQueries.GetCurrency(context.Background(), currency1.Coin)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, currency2)
+
+	require.Equal(t, currency1.ID, currency2.ID)
+	require.Equal(t, currency1.Coin, currency2.Coin)
+	require.Equal(t, currency1.Name, currency2.Name)
+}
+
+func TestListCurrencies(t *testing.T) {
+	arg := usecase.ListCurrenciesParams{
+		Limit:  10,
+		Offset: 5,
+	}
+
+	currencies, err := testQueries.ListCurrencies(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.Len(t, currencies, 10)
+
+	for _, currency := range currencies {
+		require.NotEmpty(t, currency)
+	}
+}
+
 func TestMain(m *testing.M) {
 	var err error
 
