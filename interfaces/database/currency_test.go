765f2eee96cab31d88c3f48e7fecfabe838aec62
moromin <34294957+moromin@users.noreply.github.com>
2021-12-19 22:07:49 +0900

    Update: using t.Parallel() to check lock DB

--- interfaces/database/currency_test.go
+++ interfaces/database/currency_test.go
@@ -2,26 +2,14 @@ package database_test
 
 import (
 	"blockchain-trading/entity"
-	"blockchain-trading/interfaces/database"
 	"blockchain-trading/usecase"
 	"blockchain-trading/util"
 	"context"
-	"database/sql"
-	"log"
-	"os"
 	"testing"
 
 	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/require"
 )
-
-const (
-	dbDriver = "postgres"
-	dbSource = "user=root password=secret host=localhost dbname=ohlc sslmode=disable"
-)
-
-var testRepo *database.CurrencyRepository
-var testDB *sql.DB
 
 func createRandomCurrency(t *testing.T) entity.Currency {
 	arg := usecase.ResisterCurrencyParams{
@@ -42,10 +30,14 @@ func createRandomCurrency(t *testing.T) entity.Currency {
 }
 
 func TestResisterCurrency(t *testing.T) {
+	t.Parallel()
+
 	createRandomCurrency(t)
 }
 
 func TestListCurrencies(t *testing.T) {
+	t.Parallel()
+
 	arg := usecase.ListCurrenciesParams{
 		Limit:  10,
 		Offset: 5,
@@ -59,17 +51,4 @@ func TestListCurrencies(t *testing.T) {
 	for _, currency := range currencies {
 		require.NotEmpty(t, currency)
 	}
-}
-
-func TestMain(m *testing.M) {
-	var err error
-
-	testDB, err = sql.Open(dbDriver, dbSource)
-	if err != nil {
-		log.Fatal("cannot connect to db:", err)
-	}
-
-	testRepo = &database.CurrencyRepository{Db: testDB}
-
-	os.Exit(m.Run())
 }
