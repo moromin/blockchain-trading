679adcb5d1656c602cd28afe0faceba180fc6a14
moromin <34294957+moromin@users.noreply.github.com>
2022-01-10 21:05:28 +0900

    fix: OpenTime, CloseTime to time.Time

--- entity/ohlc.go
+++ entity/ohlc.go
@@ -1,15 +1,18 @@
 package entity
 
+import "time"
+
 type OHLC struct {
+	ID                       int64
 	Symbol                   string
 	Interval                 string
-	OpenTime                 int64
+	OpenTime                 time.Time
 	Open                     float64
 	High                     float64
 	Low                      float64
 	Close                    float64
 	Volume                   float64
-	CloseTime                int64
+	CloseTime                time.Time
 	QuoteAssetVolume         float64
 	NumberOfTrades           int64
 	TakerBuyBaseAssetVolume  float64
