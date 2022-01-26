529d58df4297e1488b8aa99d55fb4ca6a93a7320
moromin <34294957+moromin@users.noreply.github.com>
2022-01-26 11:08:00 +0900

    feat: add FindBySymbol query

--- db/query/ohlc.sql
+++ db/query/ohlc.sql
@@ -10,14 +10,15 @@ INSERT INTO ohlcs (
 	volume,
 	closetime,
 	quote_asset_volume,
-	numer_of_trades,
+	number_of_trades,
 	taker_buy_base_asset_volume,
-	taker_buy_quote_asset_volume 
+	taker_buy_quote_asset_volume
 ) VALUES (
-	$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13 
+	$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13
 ) RETURNING *;
 
--- name: GetOHLC :many
-SELECT * FROM ohlcs
-WHERE symbol = $1 AND interval = $2
+-- name: FindBySymbol :many
+SELECT symbol, interval, opentime, open, high, low, close, volume
+FROM ohlcs
+WHERE symbol = $1
 ORDER BY id;
