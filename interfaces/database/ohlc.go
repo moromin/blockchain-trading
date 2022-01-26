76f10e10ef912341a344d2e43a2365ae78f8bf4f
moromin <34294957+moromin@users.noreply.github.com>
2022-01-26 14:41:29 +0900

    feat: add methos related to GetOHLCsBySymbol to do backtest

--- interfaces/database/ohlc.go
+++ interfaces/database/ohlc.go
@@ -24,9 +24,9 @@ INSERT INTO ohlcs (
 	quote_asset_volume,
 	number_of_trades,
 	taker_buy_base_asset_volume,
-	taker_buy_quote_asset_volume 
+	taker_buy_quote_asset_volume
 ) VALUES (
-	$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13 
+	$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13
 ) RETURNING *
 `
 
@@ -64,4 +64,43 @@ func (or *OHLCRepository) RegisterOHLC(ctx context.Context, arg usecase.Register
 		&i.TakerBuyQuoteAssetVolume,
 	)
 	return i, err
+}
+
+const FindBySymbol = `-- name: FindBySymbol :many
+SELECT symbol, interval, opentime, open, high, low, close, volume
+FROM ohlcs
+WHERE symbol = $1
+ORDER BY id
+`
+
+func (or *OHLCRepository) FindBySymbol(ctx context.Context, symbol string) ([]entity.OHLC, error) {
+	rows, err := or.Db.QueryContext(ctx, FindBySymbol, symbol)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	items := []entity.OHLC{}
+	for rows.Next() {
+		var i entity.OHLC
+		if err := rows.Scan(
+			&i.Symbol,
+			&i.Interval,
+			&i.OpenTime,
+			&i.Open,
+			&i.High,
+			&i.Low,
+			&i.Close,
+			&i.Volume,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
 }
